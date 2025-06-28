5c49ebdb57dc17e8a403b0efaae64fa8e6c7ef80
Judith Mbali Matsaneng <mbali.meyers@gmail.com>
2025-06-28 22:09:34 +0200

    updated file again

--- task_2.sql
+++ task_2.sql
@@ -1,17 +1,14 @@
-
--- Create the authors table
 CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT,
     author_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (author_id)
 );
 
--- Create the books table
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     author_id INT,
-    publication_date DATE,
+    publication_id DATE,
     isbn VARCHAR(20),
     price DECIMAL(10, 2),
     stock_quantity INT,
@@ -19,8 +16,7 @@ CREATE TABLE IF NOT EXISTS books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Create the customers table
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
@@ -30,25 +26,24 @@ CREATE TABLE IF NOT EXISTS customers (
     PRIMARY KEY (customer_id)
 );
 
--- Create the orders table
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT,
     customer_id INT,
     order_date DATE NOT NULL,
     total_cost DECIMAL(10, 2),
     PRIMARY KEY (order_id),
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Create the order_details table
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT AUTO_INCREMENT,
     order_id INT,
     book_id INT,
     quantity INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (order_detail_id),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
+
 
