7f182196a02b1ed45832d65bf0d63b67d3c7f076
Judith Mbali Matsaneng <mbali.meyers@gmail.com>
2025-06-28 22:24:04 +0200

    updated the script

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,21 @@
 SHOW FULL COLUMNS FROM alx_book_store.books;
 
+
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
+
+
+With SQL keywords in uppercase:
+
+
+SELECT 
+becomes SELECT stays the same 
+    COLUMN_NAME, 
+    COLUMN_TYPE 
+FROM becomes FROM is a keyword and will be uppercased to FROM
