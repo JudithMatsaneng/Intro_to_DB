e67281c9feae968f771d32ca69be3c7ea241ddcb
Judith Mbali Matsaneng <mbali.meyers@gmail.com>
2025-06-28 22:12:21 +0200

    updated customers table

--- task_2.sql
+++ task_2.sql
@@ -18,9 +18,8 @@ CREATE TABLE IF NOT EXISTS Books (
 
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE,
     phone_number VARCHAR(20),
     address TEXT,
     PRIMARY KEY (customer_id)
